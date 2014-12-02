5859e389b3fe8904b56c46e2dc3439af739e4b82
Michal Špaček <tupinek@gmail.com>
2014-12-02 09:28:35 +0100

    Fix EXAMPLE1 section in doc.

--- Warsaw.pm
+++ Warsaw.pm
@@ -81,13 +81,13 @@ For more information about Warsaw Map, click L<here|https://en.wikipedia.org/wik
  my $obj = Map::Tube::Warsaw->new;
 
  # Get route.
- my $route = $obj->get_shortest_route(decode_utf8('Люлин'), decode_utf8('Вардар'));
+ my $route = $obj->get_shortest_route(decode_utf8('Imielin'), decode_utf8('Świętokrzyska'));
 
  # Print out type.
  print "Route: ".encode_utf8($route)."\n";
 
  # Output:
- # Route: Люлин (Първи метродиаметър), Западен парк (Първи метродиаметър), Вардар (Първи метродиаметър)
+ # Route: Imielin (Linia M1), Stokłosy (Linia M1), Ursynów (Linia M1), Służew (Linia M1), Wilanowska (Linia M1), Wierzbno (Linia M1), Racławicka (Linia M1), Pole Mokotowskie (Linia M1), Politechnika (Linia M1), Centrum (Linia M1), Świętokrzyska (Linia M1)
 
 =head1 EXAMPLE2
 
--- examples/ex1.pl
+++ examples/ex1.pl
@@ -12,10 +12,10 @@ use Map::Tube::Warsaw;
 my $obj = Map::Tube::Warsaw->new;
 
 # Get route.
-my $route = $obj->get_shortest_route(decode_utf8('Люлин'), decode_utf8('Вардар'));
+my $route = $obj->get_shortest_route(decode_utf8('Imielin'), decode_utf8('Świętokrzyska'));
 
 # Print out type.
 print "Route: ".encode_utf8($route)."\n";
 
 # Output:
-# Route: Люлин (Първи метродиаметър), Западен парк (Първи метродиаметър), Вардар (Първи метродиаметър)
+# Route: Imielin (Linia M1), Stokłosy (Linia M1), Ursynów (Linia M1), Służew (Linia M1), Wilanowska (Linia M1), Wierzbno (Linia M1), Racławicka (Linia M1), Pole Mokotowskie (Linia M1), Politechnika (Linia M1), Centrum (Linia M1), Świętokrzyska (Linia M1)
