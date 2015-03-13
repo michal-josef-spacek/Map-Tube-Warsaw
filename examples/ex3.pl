14a84d1e273214ab83144edfe7940932f97c0bcf
Michal Špaček <tupinek@gmail.com>
2015-03-13 05:13:51 +0100

    Update examples/ex3.pl file.

--- examples/ex3.pl
+++ examples/ex3.pl
@@ -25,4 +25,4 @@ $g->graph('Warsaw.png');
 system "ls -l Warsaw.png";
 
 # Output like:
-# -rw-r--r-- 1 skim skim 71382 Dec 28 19:58 Warsaw.png
+# -rw-r--r-- 1 skim skim 86520 Mar 13 05:03 Warsaw.png
