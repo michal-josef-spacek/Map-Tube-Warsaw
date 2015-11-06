850d5b317491a9431311811b07b0e9fcd7f0756e
Michal Špaček <tupinek@gmail.com>
2015-11-06 09:27:49 +0100

    Add EXAMPLE5 section to doc.

--- Warsaw.pm
+++ Warsaw.pm
@@ -218,6 +218,61 @@ For more information about Warsaw Map, click L<here|https://en.wikipedia.org/wik
  # Linia M1
  # Linia M2
 
+=head1 EXAMPLE5
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(encode_utf8);
+ use Map::Tube::Warsaw;
+
+ # Arguments.
+ if (@ARGV < 1) {
+         print STDERR "Usage: $0 line\n";
+         exit 1;
+ }
+ my $line = $ARGV[0];
+
+ # Object.
+ my $obj = Map::Tube::Warsaw->new;
+
+ # Get stations for line.
+ my $stations_ar = $obj->get_stations($line);
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } @{$stations_ar};
+
+ # Output:
+ # Usage: __PROG__ line
+
+ # Output with 'foo' argument.
+ # Map::Tube::get_stations(): ERROR: Invalid Line Name [foo]. (status: 105) file __PROG__ on line __LINE__
+
+ # Output with 'Linia M1' argument.
+ # Kabaty
+ # Natolin
+ # Imielin
+ # Stokłosy
+ # Ursynów
+ # Służew
+ # Wilanowska
+ # Wierzbno
+ # Racławicka
+ # Pole Mokotowskie
+ # Politechnika
+ # Centrum
+ # Świętokrzyska
+ # Ratusz Arsenał
+ # Dworzec Gdański
+ # Plac Wilsona
+ # Marymont
+ # Słodowiec
+ # Stare Bielany
+ # Wawrzyszew
+ # Młociny
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
