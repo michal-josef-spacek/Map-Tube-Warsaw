3f9d96b2c61225549016326ed67c92aa3869ed70
Michal Josef Špaček <tupinek@gmail.com>
2020-11-03 23:22:09 +0100

    Improve LICENSE AND COPYRIGHT section in doc

--- Warsaw.pm
+++ Warsaw.pm
@@ -297,9 +297,11 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2014-2020 Michal Josef Špaček
- Artistic License
- BSD 2-Clause License
+© 2014-2020 Michal Josef Špaček
+
+Artistic License
+
+BSD 2-Clause License
 
 =head1 VERSION
 
