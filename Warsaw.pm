28b6a29d3e841356a65a0a48e9d485eaf70c3434
Michal Josef Špaček <tupinek@gmail.com>
2025-01-21 16:08:21 +0100

    Fix stations on multiple lines (GH#2)

--- Warsaw.pm
+++ Warsaw.pm
@@ -161,7 +161,7 @@ Returns string with XML.
  print "Route: ".encode_utf8($route)."\n";
 
  # Output:
- # Route: Imielin (Linia M1), Stokłosy (Linia M1), Ursynów (Linia M1), Służew (Linia M1), Wilanowska (Linia M1), Wierzbno (Linia M1), Racławicka (Linia M1), Pole Mokotowskie (Linia M1), Politechnika (Linia M1), Centrum (Linia M1), Świętokrzyska (Linia M1)
+ # Route: Imielin (Linia M1), Stokłosy (Linia M1), Ursynów (Linia M1), Służew (Linia M1), Wilanowska (Linia M1), Wierzbno (Linia M1), Racławicka (Linia M1), Pole Mokotowskie (Linia M1), Politechnika (Linia M1), Centrum (Linia M1), Świętokrzyska (Linia M1, Linia M2)
 
 =head1 EXAMPLE2
 
